be74ad20eb3fcccfa8b2334ce9c7fced7444def5
subhash.R <subhashregati1@gamil.com>
2023-01-06 12:27:27 +0530

    added the new main file extension

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 def addition(a,b):
     return a+b
-input_var=int(input("please enter \n1- for addition\n2 - for subtraction\n3 - for division \n4 - for multiplication\t"))
-a=int(input("enter 1st variable"))
-b=int(input("enter 2nd variable"))
+input_var_1=1
+a=11
+b=11
 ans=0
-if(input_var==1):
+if(input_var_1==1):
     ans=addition(a,b)
 else:
     ans="None"
