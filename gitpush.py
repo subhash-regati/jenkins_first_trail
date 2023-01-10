5a59a751e4d8d8be359d1db7b26db374051f6f0f
subhash-regati <119586855+subhash-regati@users.noreply.github.com>
2023-01-10 16:31:23 +0530

    Update gitpush.py
    updated gitpush command

--- gitpush.py
+++ gitpush.py
@@ -1,11 +1,12 @@
 from git import Repo
-editorname=input()
+import os
+editorname=os.getenv("editorname")
 full_local_path = "C:\\Users\\subhash.reddy\\PycharmProjects\\trail"
 username = "subhash-regati"
 password = "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"
 remote = f"https://{username}:{password}@github.com/subhash-regati/jenkins_first_trail.git"
 repo = Repo(full_local_path)
-repo.git.add('C:\\Users\\subhash.reddy\\PycharmProjects\\trail\\gitpush.py')
+repo.git.add('C:\\Users\\subhash.reddy\\PycharmProjects\\trail\\hello3.txt')
 repo.index.commit("added new text file by " + editorname)
 repo = Repo(full_local_path)
 origin = repo.remote(name="origin")
