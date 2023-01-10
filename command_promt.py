65044935650d1adfd7344211272ae72b41e008e9
subhash.R <subhashregati1@gamil.com>
2023-01-10 11:55:20 +0530

    added a new file

---
+++ command_promt.py
@@ -0,0 +1,29 @@
+import subprocess
+
+def run_win_cmd(cmd):
+    result = []
+    process = subprocess.Popen(cmd,
+                               shell=True,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+    for line in process.stdout:
+        result.append(line)
+    errcode = process.returncode
+    for line in result:
+        print(line)
+    if errcode is not None:
+        raise Exception('cmd %s failed, see above for details', cmd)
+cmd="main.py"
+run_win_cmd("git init")
+print("\n")
+run_win_cmd("git add --all")
+print("\n")
+run_win_cmd("git status")
+print("next step")
+run_win_cmd('git commit -m "added a new file"')
+run_win_cmd("git remote add origin https://github.com/subhash-regati/jenkins_first_trail.git")
+run_win_cmd("git remote -v")
+run_win_cmd("git push origin master")
+print("run the command")
+run_win_cmd("git status")
+print("\n")
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def addition(a,b):
     return a+b
 input_var_1=1
 a=11
-b=11
+b=231
 ans=0
 if(input_var_1==1):
     ans=addition(a,b)
