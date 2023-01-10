6c4dda8a571a258b031d1f296ba0bb6c745ad99f
subhash.R <subhashregati1@gamil.com>
2023-01-10 16:13:03 +0550

    added new text file by subhash

--- gitpush.py
+++ gitpush.py
@@ -1,13 +1,12 @@
 from git import Repo
-
-full_local_path = "C:\Users\subhash.reddy\PycharmProjects\jenkins_first_trail"
-username = "your-username"
-password = "your-password"
-remote = f"https://{'subhash-regati'}:{'PropertyRemoveThis@1997'}@github.com/subhash-regati/jenkins_first_trail.git"
-Repo.clone_from(remote, full_local_path)
+editorname=input()
+full_local_path = "C:\\Users\\subhash.reddy\\PycharmProjects\\trail"
+username = "subhash-regati"
+password = "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"
+remote = f"https://{username}:{password}@github.com/subhash-regati/jenkins_first_trail.git"
 repo = Repo(full_local_path)
-repo.git.add("C:\Users\subhash.reddy\PycharmProjects\jenkins_first_trail\hello.txt")
-repo.index.commit("new text file")
+repo.git.add('C:\\Users\\subhash.reddy\\PycharmProjects\\trail\\gitpush.py')
+repo.index.commit("added new text file by " + editorname)
 repo = Repo(full_local_path)
 origin = repo.remote(name="origin")
 origin.push()
